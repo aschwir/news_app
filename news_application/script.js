3d74e065db310bdd0c3020f68eebd2b8865c4b17
andrew schwirzinski <aschwirzinski@gmail.com>
2022-12-15 16:38:02 -0500

    render html articles on page

--- news_application/script.js
+++ news_application/script.js
@@ -7,43 +7,37 @@
 // https://newsapi.org/v2/everything?q=Apple&from=2022-12-14&sortBy=popularity&apiKey=API_KEY
 
 const container = document.querySelector(".article-box");
-// console.log(container);
-const articleArr = [];
-
-// const article = {
-//   author: "",
-//   title: "",
-//   description: "",
-//   url: "",
-//   urlToImage: "",
-//   publishedAt: "",
-//   content: "",
-// };
 
 const news = async function () {
-  let articleOBJ = {};
-
   const results = await fetch(
     `https://newsapi.org/v2/top-headlines?country=us&apiKey=c300c0a2777c47458b27d4880fbb95c8`
   );
-
   const data = await results.json();
-  data.articles.map((el) => articleArr.push(el));
+  const articleArr = data.articles;
+  articleArr.forEach((el) => {
+    if (!el.author || el.author.startsWith("http")) return;
+
+    let date = new Date(el.publishedAt).toLocaleString();
+
+    const markup = `
+    <article class="article-container">
+      <span class="title">${el.title}</span>
+      <span class="img"><img src="${el.urlToImage}" alt="image"></span>
+      <span class="author">${el.author}</span>
+      <span class="description">${el.description}</span>
+      <span class="publishDate">${date}</span>
+     
+    </article>
+    `;
+
+    console.log(date);
+
+    addHTML(markup);
+  });
 };
 
 news();
 
-const markup = `
-    <article class="article-container">
-      <span class="title">Title</span>
-      <span class="img">url to image</span>
-      <span class="author">author</span>
-      <span class="description">description</span>
-      <span class="publishDate">published at</span>
-      <span class="content">content</span>
-    </article>
-`;
-
-const addHTML = function () {
+const addHTML = function (markup) {
   container.insertAdjacentHTML("beforeend", markup);
 };
