76bb28f407afc020d3d1e74c8a60f323ce1ef752
andrew schwirzinski <aschwirzinski@gmail.com>
2022-12-24 18:49:41 -0500

    finished news search functionality

--- news_application/script.js
+++ news_application/script.js
@@ -1,43 +1,40 @@
-let keyword;
-//; API KEY
-const KEY = "c300c0a2777c47458b27d4880fbb95c8";
-//search top headlines
-const countryURL = `https://newsapi.org/v2/top-headlines?country=us&apiKey=${KEY}`;
-// query news by keyword
-const keywordURL = ` https://newsapi.org/v2/everything?q=${this.keyword}&language=en&sortBy=popularity&apiKey=${KEY}`;
-
 const container = document.querySelector(".article-box");
 const searchBtn = document.querySelector(".searchBtn");
+const inputField = document.querySelector("#search_keyword");
 
-const news = async function () {
-  // let url = !keyword ? countryURL : keywordURL;
+let searchTerm;
 
-  const results = await fetch(countryURL);
-  const data = await results.json();
-  const articleArr = data.articles;
-  articleArr.forEach((el) => {
-    if (!el.author || el.author.startsWith("http")) return;
+// const currentDate = new Date();
+// const startDate = new Date(currentDate.setDate(currentDate.getDate() - 5));
+// console.log(new Date(startDate.toISOString()));
 
+//API KEY
+const newsAPIKey = "c300c0a2777c47458b27d4880fbb95c8";
+const newsAPIURL = `https://newsapi.org/v2/top-headlines?country=us&apiKey=${newsAPIKey}`;
+const keywordSearchURL = `https://newsapi.org/v2/everything?q=&apiKey=${newsAPIKey}`;
+
+const news = async function (url) {
+  const response = await fetch(url);
+  const data = await response.json();
+  const articles = data.articles;
+
+  articles.forEach((el) => {
+    if (!el.urlToImage) el.urlToImage = "defaultNew.png";
+    if (!el.author) return;
     let date = new Date(el.publishedAt).toLocaleString();
-
-    for (let i = 100; i < el.description.length; i++) {
-      if (el.description[i] === " ") {
-        el.description = el.description.slice(0, i) + "...";
-      }
-    }
 
     const markup = `
     <article class="article-container">
     <div class="title-div">
       <h1 class="title">${el.title}</h1>
       </div>
-      <span class="img"><a href="${el.url}" target="_blank"><img src="${el.urlToImage}" alt="image"></a></span>
+      <span class="img"><a href="${el.url}" target="_blank"><img src="
+      ${el.urlToImage}" alt="image"></a></span>
       <span class="description">${el.description}</span>
       <div class="article-footer"
       <span class="author"> Author: ${el.author}</span>
       <span class="publishDate">Published: ${date}</span>
       </div>
-
     </article>
     `;
 
@@ -45,13 +42,15 @@ const news = async function () {
   });
 };
 
-news("Trump");
-/////////////////////////////////////////////////////////////////////////////
+news(newsAPIURL);
 
 const addHTML = function (markup) {
   container.insertAdjacentHTML("beforeend", markup);
 };
 
-searchBtn.addEventListener("click", function () {
-  news();
+searchBtn.addEventListener("click", function (e) {
+  e.preventDefault();
+  let url = keywordSearchURL.replace("q=", `q=${inputField.value}`);
+  container.innerHTML = " ";
+  news(url);
 });
